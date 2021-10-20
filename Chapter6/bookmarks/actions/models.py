c2463a54598f671232eeabf4c0695457ba97d3e9
Slimjim821 <72995359+Slimjim821@users.noreply.github.com>
2021-10-19 20:08:34 -0500

    Add files via upload

--- Chapter6/bookmarks/actions/models.py
+++ Chapter6/bookmarks/actions/models.py
@@ -1,38 +1,25 @@
 from django.db import models
-from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
-
-    def __str__(self):
-        return f'Profile for user {self.user.username}'
-
-
-class Contact(models.Model):
-    user_from = models.ForeignKey('auth.User',
-                                  related_name='rel_from_set',
+class Action(models.Model):
+    user = models.ForeignKey('auth.User',
+                             related_name='actions',
+                             db_index=True,
+                             on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
                                   on_delete=models.CASCADE)
-    user_to = models.ForeignKey('auth.User',
-                                related_name='rel_to_set',
-                                on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True,
+                                            db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
     created = models.DateTimeField(auto_now_add=True,
                                    db_index=True)
 
     class Meta:
         ordering = ('-created',)
-
-    def __str__(self):
-        return f'{self.user_from} follows {self.user_to}'
-
-
-# Add following field to User dynamically
-user_model = get_user_model()
-user_model.add_to_class('following',
-                        models.ManyToManyField('self',
-                                                through=Contact,
-                                                related_name='followers',
-                                                symmetrical=False))
