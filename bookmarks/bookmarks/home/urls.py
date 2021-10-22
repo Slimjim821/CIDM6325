d64d593c7dc9658b23ab0469d0136fcc864ae65e
Slimjim821 <jim.n.johnson16@gmail.com>
2021-10-22 12:25:54 -0500

    Push github files

--- bookmarks/bookmarks/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-# from django.contrib.auth import views as auth_views
-
-from . views import HomeView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-]
--- t100data/t100data/air_carrier_market_data/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AirCarrierMarketDataConfig(AppConfig):
-    name = 'air_carrier_market_data'
